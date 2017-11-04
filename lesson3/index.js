20bac39e88a5e30cb678beb06cb658d95dd94ffc
adolph <1138027478@qq.com>
2017-11-04 17:31:43 +0800

    添加

---
+++ lesson3/index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+
+var app = express();
+
+var superagent = require('superagent');
+
+var cheerio = require('cheerio');
+
+app.get('/',(request,response,next)=>{
+    superagent.get('https://cnodejs.org/')
+        .end((err,result)=>{
+            if(err){
+                return next(err);
+            }else{
+                var $ = cheerio.load(result.text);
+                var item = [];
+                $('#topic .topic_title').map((index,element) => {
+                    item.push({
+                        'title':$(element).attr('title'),
+                        'href':$(element).attr('href'),
+                        'author':$
+                    });
+                });
+            }
+    });
+});
+
+
+
+var server = app.listen(3000,() => {
+    var address = server.address().address;
+    var port = server.address().port;
+
+    console.dir(`express run ${address}:${port}`);
+});
